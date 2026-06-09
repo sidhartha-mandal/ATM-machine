5c015467e3e5d18768dacc40a1444a440f44c62e
sidhartha-mandal <msidhartha10@gmail.com>
2026-06-10 00:25:53 +0530

    setup base code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int atmnumber = 12345;
+        int atmpin = 1234;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your ATM number : ");
+        int atmNumber = sc.nextInt();
+        System.out.print("enter your ATM PIN : ");
+        int atmPin = sc.nextInt();
+        if ((atmNumber == atmnumber) &&(atmPin==atmpin)) {
+
+            while(true){
+                System.out.println("1. Available the Balance. \n 2. Withdraw the Amount. \n 3. Deposit the amount. \n 4. View Mini Statement. \n 5. EXIT the program");
+                System.out.println("Enter the choice: ");
+                int ch = sc.nextInt();
+            }
+        }else{
+            System.out.println("Incorrct ATM pin or Number..");
+            System.exit(0);
+        }
     }
 }
