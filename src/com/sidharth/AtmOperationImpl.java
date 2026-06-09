ddf16c6d08a8764a6ea6df253b1fa3da319d792d
sidhartha-mandal <msidhartha10@gmail.com>
2026-06-10 00:54:23 +0530

    adding interface methods to the class

--- src/com/sidharth/AtmOperationImpl.java
+++ src/com/sidharth/AtmOperationImpl.java
@@ -1,5 +1,24 @@
 package com.sidharth;
 
-public class AtmOperationImpl {
+public class AtmOperationImpl implements AtmOperationInterface{
 
+    @Override
+    public void ViewBalance() {
+
+    }
+
+    @Override
+    public void withdrawAmount(double withdrawAmount) {
+
+    }
+
+    @Override
+    public void depositAmount(double withdrawAmount) {
+
+    }
+
+    @Override
+    public void viewMiniStatement() {
+
+    }
 }
